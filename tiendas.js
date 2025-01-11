cb5e844f726a930f1619020ab7457c9e7fd660da
milenka1601 <milenka.quisbert.baldivieso@gmail.com>
2025-01-11 14:13:06 -0400

    Add files via upload

---
+++ tiendas.js
@@ -0,0 +1,12 @@
+[
+  {"tienda": "pedrini.net", "ventas": 5500, "ranking": 8},
+  {"tienda": "tiendaLuces.com", "ventas": 4000, "ranking": 6},
+  {"tienda": "Compras.net", "ventas": 1200, "ranking": 5},
+  {"tienda": "AnitaTiendas.com", "ventas": 800, "ranking": 3},
+  {"tienda": "alibaba.com", "ventas": 8000, "ranking": 1},
+  {"tienda": "2x1.com", "ventas": 3750, "ranking": 2},
+  {"tienda": "LauraFloresTienda.com", "ventas": 8564, "ranking": 7},
+  {"tienda": "Marta.net", "ventas": 1985, "ranking": 4},
+  {"tienda": "tiendasQuisbertBal.net", "ventas": 7010, "ranking": 10},
+  {"tienda": "tiendasTomas.net", "ventas": 850, "ranking": 9}
+]
